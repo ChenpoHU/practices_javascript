ffabb8158343d1e3c0f61a86dc3652bdfb1a44ee
Charlotte Chenpo Hu <chenpo.charlotte.hu@gmail.com>
2023-02-22 20:56:22 +0100

    var vs. let vs. const
    - var declarations are globally scoped or function scoped while let and const are block scoped.
    - var variables can be updated and re-declared within its scope; let variables can be updated but not re-declared; const variables can neither be updated nor re-declared.
    - They are all hoisted to the top of their scope. But while var variables are initialized with undefined, let and const variables are not initialized.
    - While var and let can be declared without being initialized, const must be initialized during declaration.

--- 4_variaveis/js/scripts.js
+++ 4_variaveis/js/scripts.js
@@ -1,43 +1,48 @@
 var teste = 1;
+/*
+- var declarations are globally scoped or function scoped while let and const are block scoped.
+- var variables can be updated and re-declared within its scope; let variables can be updated but not re-declared; const variables can neither be updated nor re-declared.
+- They are all hoisted to the top of their scope. But while var variables are initialized with undefined, let and const variables are not initialized.
+- While var and let can be declared without being initialized, const must be initialized during declaration.*/
 
-var nome = "João";
+var name = "João";
 
-console.log(teste);
+console.log(test); //undefined
 
-teste = 'Matheus';
+test = 'Matheus';
 
-console.log(teste);
+console.log(test); //Matheus
 
-console.log(nome);
+console.log(name);
 
-var $nome = 'ASD';
-var _nome = 'ASD2';
+var $name = 'ASD';
+var _name = 'ASD2';
 
-console.log($nome);
-console.log(_nome);
+console.log($name);
+console.log(_name);
 
-var nome5 = 'ASD3';
+var name5 = 'ASD3';
 
-console.log(nome5);
+console.log(name5);
 
-var meuPrimeiroNome = 'Matheus';
+var myFirstName = 'Matheus';
 
-console.log(meuPrimeiroNome); // camelCase
+console.log(myFirstName); // camelCase
 
-var meusobrenome = "Battisti";
+var myLastName = "Battisti"; // global or function scope
 
-console.log(meusobrenome);
+console.log(myLastName);
 
-let testando = 1;
-const ola = 2;
+let testing = 1; // block-scope, e.g., if, else
+const ola = 2; // block-scope, will not change
 
-console.log(testando);
+console.log(testing);
 console.log(ola);
 
-var meuNome;
+var myName;
 
-console.log(meuNome);
+console.log(myName); //undefined
 
-meuNome = "Matheus";
+myName = "Matheus";
 
-console.log(meuNome);
+console.log(myName); //Matheus
