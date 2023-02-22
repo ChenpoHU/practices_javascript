aed117915fac42f1451385c52639293d3d027dd5
Charlotte Chenpo Hu <chenpo.charlotte.hu@gmail.com>
2023-02-22 21:26:30 +0100

    Update scripts.js

--- 7_boolean/js/scripts.js
+++ 7_boolean/js/scripts.js
@@ -1,12 +1,12 @@
-var verdadeiro = true;
+var verdadeiro_true = true; //true cannot be used directly as a variable name
 
-console.log(verdadeiro);
-console.log(typeof verdadeiro);
+console.log(verdadeiro_true);
+console.log(typeof verdadeiro_true); //boolean
 
 console.log(typeof true);
 console.log(typeof false);
 
 var falso = false;
 
-console.log(falso);
+console.log(falso); //false
 console.log(typeof falso);
