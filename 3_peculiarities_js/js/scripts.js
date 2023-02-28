e2aefaf9df69893dc54188e531ae248afcf0fad2
ChenpoHU <chenpo.charlotte.hu@gmail.com>
2023-02-28 11:42:04 +0100

    add comments and modify the language to portuguese

---
+++ 3_peculiarities_js/js/scripts.js
@@ -0,0 +1,19 @@
+console.log("First");
+
+console.log("Last");
+
+console.log("One more console.log");
+
+// Declare a variable
+var a = 1;
+
+a = 'test';
+
+console.log(a); // test
+
+// This is a comment
+
+/*
+  This 
+  is multi-line comment
+*/
